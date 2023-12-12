5923fe176f5d8bdf9250e24fda3a8bcd1d69bccc
Salar Nili <77540189+DesertFoox@users.noreply.github.com>
2023-12-12 17:19:53 +0330

    add find user

--- src/components/common/input.tsx
+++ src/components/common/input.tsx
@@ -19,7 +19,7 @@ const Input: React.FC<IInputProps> = ({ label, type, className, placeHolder, onC
                     name={type}
                     id={type}
                     onChange={onChange}
-                    className={`px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${className}`}
+                    className={`px-3 block w-full rounded-md border-0 py-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${className}`}
                     placeholder={placeHolder}
                 />
             </div>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,55 @@
-import React from 'react';
+import { useState } from 'react';
+
+import { Octokit } from 'octokit'
+
 import githubImage from '../../assets/images/githubl.png';
 import Layout from '../../components/header/layout/layout';
 import Input from '../../components/common/input';
 
+const octokit = new Octokit({
+    auth: "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",
+});
+
 const Home = () => {
+    const [username, setUsername] = useState<string>("");
+    const [loading, setLoading] = useState<boolean>(false)
+    const getUserData = async () => {
+        setLoading(true)
+        try {
+            const response = await octokit.request('GET /users/{username}/repos', {
+                username: username
+            });
+            console.log(response.data);
+        } catch (error) {
+            console.error("Error fetching user repositories:", error);
+        }
+        setLoading(false)
+    }
+
+
+
     return (
         <Layout>
-            <div className='flex flex-col justify-center items-center h-screen'> 
+            <div className='flex flex-col justify-center items-center h-screen'>
                 <div>
                     <img className='w-[300px] rounded-md' src={githubImage} alt="Github" />
                 </div>
                 <div className='mt-4'>
-                    <Input labelClassName='text-xl' label={'Enter a Username To Search 🔎'} type={'search'} placeHolder={'search a user, like DesertFoox'} />
-                    <button className='px-2 py-2 bg-[#1F2937] text-white rounded-md mt-3'>Search</button>
+                    <Input
+                        labelClassName='text-xl'
+                        label={'Enter a Username To Search 🔎'}
+                        type={'search'}
+                        placeHolder={'search a user, like DesertFoox'}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <button
+                        className='px-2 py-2 bg-[#1F2937] text-white rounded-md mt-3'
+                        disabled={loading}
+                        onClick={getUserData}
+                    >
+
+                        {loading ? <span className='animate-ping'>🔍</span> : 'Search'}
+                    </button>
                 </div>
             </div>
         </Layout>
