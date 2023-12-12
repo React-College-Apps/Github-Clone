dd62c804aa1d948b249525d6a9a5cced054421d5
Salar Nili <77540189+DesertFoox@users.noreply.github.com>
2023-12-12 19:03:36 +0330

    add userCustomHook

---
+++ src/context/User.context.tsx
@@ -0,0 +1,36 @@
+// src/contexts/UserContext.tsx
+import React, { createContext, useState, useContext, ReactNode } from 'react';
+
+
+
+type UserContextType = {
+    user: any;
+    setUser: React.Dispatch<React.SetStateAction<any>>;
+};
+const UserContext = createContext<UserContextType>({
+    user: {},
+    setUser: () => { },
+});
+
+type UserProviderProps = {
+    children: ReactNode;
+};
+
+export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
+    const [user, setUser] = useState<any>(null);
+
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            {children}
+        </UserContext.Provider>
+    );
+};
+
+
+export const useUser = () => {
+    const context = useContext(UserContext);
+    if (!context) {
+        throw new Error('useUser must be used within a UserProvider');
+    }
+    return context;
+};
