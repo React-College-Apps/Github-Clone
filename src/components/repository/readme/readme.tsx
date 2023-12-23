cee1b177e01fbd18cfa3f870c1fcd919b550e5e5
Salar Nili <77540189+DesertFoox@users.noreply.github.com>
2023-12-23 12:48:14 +0330

    add readme header

--- src/components/repository/readme/readme.tsx
+++ src/components/repository/readme/readme.tsx
@@ -1,6 +1,6 @@
 // Readme.tsx
 import React from 'react';
-import {marked } from 'marked';
+import { marked } from 'marked';
 import DOMPurify from 'dompurify';
 
 interface IReadMe {
@@ -12,10 +12,14 @@ const Readme: React.FC<IReadMe> = ({ readme }) => {
 
     const sanitizedHTML = DOMPurify.sanitize(markdown);
 
-    return (
+    return (<>
+
         <div className='ml-10 mt-7 border border-gray-300 p-5 rounded-lg w-[60rem]'
-             dangerouslySetInnerHTML={{ __html: sanitizedHTML }}>
+        >
+            <h2 className='text-3xl mb-2'>README.md</h2>
+            <div dangerouslySetInnerHTML={{ __html: sanitizedHTML }}></div>
         </div>
+    </>
     );
 };
 
